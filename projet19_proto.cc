1833f148725ede06fc07290b05e40e09502f140e
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-12-01 18:39:02 +0100

    New Version

--- projet19_proto.cc
+++ projet19_proto.cc
@@ -1,11 +1,8 @@
 /** \author PELLERIN Guillaume
  *  \date	dec 2018
  */
-#include <fstream>
 #include <iostream>
 #include <math.h>
-#include <string>
-
 using namespace std;
 
 void error_nbR(int nbR);
@@ -13,331 +10,178 @@ void error_color(int id);
 void error_threshold(double invalid_val);
 void error_nb_filter(int nb_filter);
 
-// FAIRE LES PROTO
+void filtering(int xPos, int yPos, int xSize, int ySize, int nbR, int** map);
 
-string readLine(string file, uint16_t line) {
-    ifstream flux(file, ios::in);
-    string data;
-    for (uint16_t i = 0; i < line; i++) {
-        getline(flux, data);
-    }
-    flux.close();
-    return (data);
-}
-
-void readDataLineDouble(string file, uint16_t line, uint16_t startPosition,
-                        uint16_t column, double data[]) {
-    ifstream flux(file, ios::in);
-    string tmpData;
-    string tmpData2;
-    uint16_t dataM = startPosition;
-    bool lastWasSpace = false;
-    for (uint16_t i = 0; i < line; i++) {
-        getline(flux, tmpData);
-    }
-    for (uint16_t i = 0; i < tmpData.size(); i++) {
-        if (tmpData[i] == ' ') {
-            if (lastWasSpace == false) {
-                data[dataM] = stod(tmpData2);
-                cout << data[dataM] << endl;
-                if (dataM > 0) {
-                    if (data[dataM] < data[dataM - 1]) {
-                        error_threshold(data[dataM]);
-                    }
-                }
-
-                tmpData2.clear();
-                dataM++;
-                lastWasSpace = true;
-            }
-        } else if (tmpData[i] == '\n') {
-            data[dataM] = stod(tmpData2);
-            cout << data[dataM] << endl;
-            if (dataM > 0) {
-                if (data[dataM] < data[dataM - 1]) {
-                    error_threshold(data[dataM]);
-                }
-            }
-            lastWasSpace = true;
-            // BREAK
-        } else {
-            lastWasSpace = false;
-            tmpData2 = tmpData2 + tmpData[i];
-        }
-    }
-    flux.close();
-}
-
-void readDataLineInt(string file, uint16_t line, uint16_t startPosition,
-                     uint16_t column, unsigned int data[][3]) {
-    ifstream flux(file, ios::in);
-    string tmpData;
-    string tmpData2;
-    uint16_t lastPosition = 0;
-    uint16_t dataM = startPosition;
-    uint16_t dataN = 0;
-    bool lastWasSpace = false;
-
-    for (uint16_t i = 0; i < line; i++) {
-        getline(flux, tmpData);
-    }
-    tmpData += ' ';
-    for (uint16_t i = 0; i < tmpData.size(); i++) {
-        if (tmpData[i] == ' ') {
-            if (dataN < column - 1) {
-                if (lastWasSpace == false) {
-                    data[dataM][dataN] = (unsigned int)stoi(tmpData2);
-                    if (data[dataM][dataN] > 255) {
-                        error_color(dataM);
-                        data[dataM][dataN] = 255;
-                    }
-                    tmpData2.clear();
-                    dataN++;
-                    lastWasSpace = true;
-                }
-            } else
-
-            {
-                if (lastWasSpace == false) {
-                    data[dataM][dataN] = (unsigned int)stoi(tmpData2);
-                    if (data[dataM][dataN] > 255) {
-                        error_color(dataM);
-                        data[dataM][dataN] = 255;
-                    }
-                    tmpData2.clear();
-                    dataN = 0;
-                    dataM++;
-                    lastWasSpace = true;
-                }
-            }
-        } else if (tmpData[i] == '\n') {
-            data[dataM][dataN] = (unsigned int)stoi(tmpData2);
-            if (data[dataM][dataN] > 255) {
-                error_color(dataM);
-                data[dataM][dataN] = 255;
-            }
-        } else {
-            lastWasSpace = false;
-            tmpData2 = tmpData2 + tmpData[i];
-        }
-    }
-
-    flux.close();
-}
-
-void getSize(string file, int* size) {
-    ifstream flux(file, ios::in);
-
-    string data = {};
-    string dataTmp = {};
-    do {
-        getline(flux, data);
-    } while (data != "P3");
-
-    getline(flux, data);
-    uint8_t j = 1;
-    data += ' ';
-    bool lastWasSpace = false;
-    for (uint8_t i = 0; i < data.size(); i++) {
-        if ((data[i] != ' ') && (data[i] != '\n')) {
-            dataTmp += data[i];
-            lastWasSpace = false;
-        } else if (lastWasSpace) {
-        } else {
-            size[j] = stoi(dataTmp);
-            lastWasSpace = true;
-            dataTmp = {};
-            j--;
-        }
-    }
-    flux.close();
-}
-
-void thresholding(int* size, string file, double colors_threshold[], uint8_t nbR,
-                  int** thresholded) {
-    ifstream flux(file, ios::in);
-
-    string data = {};
-    string dataTmp = {};
-
-    do {
-        getline(flux, data);
-    } while (data != "P3");
-
-    getline(flux, data);
-
-    getline(flux, data);
-    int max = stoi(data);
-    int R = 0, G = 0, B = 0;
-
-    int arrayTmp[size[1] * 3] = {};
-    getline(flux, data);
-
-    double tmpDouble = 0;
-
-    for (uint16_t i = 0; i < size[0]; i++) {
-        uint8_t x = 0;
-        data += ' ';
-        dataTmp = "";
-        bool lastWasSpace = false;
-        // cout << data << endl;
-        int beginTMP = 0;
-        while (data[beginTMP] == ' ') { // ATTENTION SI " " EN 1ER !!! RAJOUTER PARTOUT
-            beginTMP++;
-        }
-        for (uint8_t k = beginTMP; k < data.size(); k++) {
-            // cout << "dataTmp " << dataTmp[0] << " " << dataTmp[1] << " " <<
-            // dataTmp[2]
-            //<< endl;
-
-            if ((data[k] != ' ') && (data[k] != '\n')) {
-                dataTmp += data[k];
-                lastWasSpace = false;
-            } else if (lastWasSpace) {
-            } else {
-                arrayTmp[x] = stoi(dataTmp);
-                // cout << "arrayTmp" << arrayTmp[x] << endl;
-                lastWasSpace = true;
-                dataTmp = {};
-                x++;
-            }
-        }
-        for (int j = 0; j < size[1]; j++) {
-            R = arrayTmp[3 * j];
-            G = arrayTmp[3 * j + 1];
-            B = arrayTmp[3 * j + 2];
-            tmpDouble = sqrt(R * R + G * G + B * B) / (sqrt(3) * max);
-            for (int k = 0; k < nbR; k++) {
-                if (tmpDouble >= colors_threshold[k] &&
-                    tmpDouble <= colors_threshold[k + 1]) {
-                    thresholded[i][j] = k + 1;
-                    k = nbR;
-                }
-            }
-        }
-        getline(flux, data);
-    }
-    flux.close();
-}
-
-void rec_filtering(uint16_t xSize, uint16_t ySize, uint16_t xPos, uint16_t yPos,
-                   void* picture) {
-    int(*p_picture)[xSize][ySize] = (int(*)[xSize][ySize])picture;
-    int color[3][3] = {};
-    // cout << xSize << ySize << xPos << yPos << endl;
-    uint8_t sameColor = 0;
-    bool isSameColor = false;
-    int testColor = -1;
-    if (xPos == 0 || xPos == xSize) {
-        // cout << "nothing" << endl;
-    } else if (yPos == 0 || yPos == ySize) {
-        // cout << "nothing" << endl;
-    } else {
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == xPos && j == yPos) {
-
-                } else {
-                    rec_filtering(xSize, ySize, xPos + i, yPos + j, *p_picture);
-
-                    if (testColor == color[i + 1][j + 1]) {
-                        sameColor++;
-                    } else {
-                        testColor = color[i + 1][j + 1];
-                    }
-                }
-            }
-        }
-        if (sameColor == 7) {
-            *p_picture[xPos][yPos] = color[0][0];
-        } else {
-            *p_picture[xPos][yPos] = 0;
-        }
-    }
-}
-
-void writeData(string file, int max, uint16_t xSize, uint16_t ySize, int** picture) {
-    ofstream flux(file, ios::out | ios::trunc);
-
-    flux << "P3" << endl;
-    flux << ySize << " " << xSize << endl;
-    flux << max << endl;
-    cout << endl;
-
-    for (int i = 0; i < xSize; i++) {
-        for (int j = 0; j < ySize; j++) {
-            if (j == ySize - 1) {
-                flux << picture[i][j] << endl;
-                cout << picture[i][j] << endl;
-            } else {
-                flux << picture[i][j] << " ";
-                cout << picture[i][j] << "     ";
-            }
-        }
-    }
-    flux.close();
-}
-
-int main() {
-
-    string file = "tests/elementary/E03.txt";
-    // Nombre Couleurs reduites
-    unsigned int nbR = (unsigned int)stoi(readLine(file, 1));
-    if (nbR < 2) {
+int main()
+{
+	int nbR = 0;
+	cin >> nbR;
+	if (nbR < 2) {
         error_nbR(nbR);
+		return(1);
     }
-    // Couleurs reduites utilisees
-    unsigned int colors_used[nbR + 1][(unsigned int)3];
+ 	// ---------------------------------------------- //
+	int colors_used[nbR + 1][3] = {0};
+	for (int i = 1; i < nbR + 1; i++){
+		for (int j = 0; j < 3; j++){
+			cin >> colors_used[i][j];
+		}
+	}
 
-    readDataLineInt(file, 2, 1, 3,
-                    colors_used); // PASSER NBR au lieu de juste lire une ligne
-                                  // ET RENVOYER LIGNE ACTUELLE
-
-    // Recuperation seuillage
-    double colors_threshold[nbR + 1];
-    colors_threshold[0] = 0;
+	// ----------------------------------------------- //
+	double colors_threshold[nbR + 1];
+	colors_threshold[0] = 0;
     colors_threshold[nbR] = 1;
-    readDataLineDouble(file, 3, 1, nbR, colors_threshold);
+	for (int i = 1; i < nbR; i++){
+		cin >> colors_threshold[i];
+	}
 
-    // Seuillage de l'image
+	// ----------------------------------------------- //
+	int nbF = 0;
+	cin >> nbF;
 
-    int size[2] = {};
+	// ----------------------------------------------- //
+	char tmpChar[2];
+	cin >> tmpChar;
+	cout << tmpChar << endl;
 
-    getSize(file, size);
+	// ----------------------------------------------- //
 
-    int** picture = new int*[size[0]];
-    for (int i = 0; i < size[0]; i++) {
-        picture[i] = new int[size[1]];
+	int size[2] = {0,0};
+	cin >> size[1] >> size[0];
+	cout << size[0] << " " << size[1] << endl;
+
+	// ----------------------------------------------- //
+
+	int max = 0;
+	cin >> max;
+	cout << max << endl;
+
+	// ----------------------------------------------- // Seuillage
+
+	int picture[size[0]][size[1]][3];
+
+	// int map[size[0]][size[1]];
+	int** map = new int*[size[0]];
+   	for (int i = 0; i < size[0]; i++) {
+    	map[i] = new int[size[1]];
     }
-    thresholding(size, file, colors_threshold, nbR, picture);
 
-    for (int i = 0; i < size[0]; i++) {
-        for (int j = 0; j < size[1]; j++) {
-            cout << picture[i][j] << "     ";
+	double tmpDouble;
+	int R = 0;
+	int G = 0;
+	int B = 0;
+	bool notTheEnd = true;
+	int k = 1;
+
+	for (int i = 0; i < size[0]; i++){
+		for (int j = 0; j < size[1]; j++){
+			for (int k = 0; k < 3; k++){
+				cin >> picture[i][j][k];
+			}
+			R = picture[i][j][0];
+			G = picture[i][j][1];
+    		B = picture[i][j][2];
+			tmpDouble = sqrt(R * R + G * G + B * B) / (sqrt(3) * max);
+			notTheEnd = true;
+			k = 1;
+			while (k <= nbR && notTheEnd){
+				if(tmpDouble < colors_threshold[k]){
+					map[i][j]=k;
+					notTheEnd = false;
+				}
+				k++;
+			}
+
+		}
+	}
+
+	// ----------------------------------------------- // Filtrage
+	
+	for (int p = 0; p < nbF; p++){
+		filtering(1,1,size[0], size[1], nbR, map); // Commencer en 0 0 ??!
+	}
+
+	if (nbF > 0){
+		for (int i = 0; i < size[0]; i++){
+			map[i][0] = 0;
+			map[i][size[1]-1] = 0; 
+		}
+		for (int j = 1; j < size[1] - 1; j++){
+			map[0][j] = 0;
+			map[size[0] - 1][j] = 0; 
+		}
+	}
+
+	// ----------------------------------------------- // Renvoie
+
+	for (int i = 0; i < size[0]; i++){
+		for (int j = 0; j < size[1]; j++){
+			for (int k = 0; k < 3; k++){
+				cout << colors_used[map[i][j]][k] << " ";
+			}
+			cout << "    ";
+		}
+		cout << endl;
+	}
+	
+
+	return 0;
+}
+
+void filtering(int xPos, int yPos, int xSize, int ySize, int nbR, int** map){
+	int maxColorNb = 0;
+	int maxColorValue = 0;
+	int testColor[nbR + 1] = {0};
+
+	if (xPos == xSize - 1 || yPos == ySize - 1|| xPos == 0 || yPos == 0){
+		// Do nothing
+	}
+	else{
+		for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+
+                } else if (i >= 0 && j >= 0){
+                    filtering(xPos + i, yPos + j, xSize, ySize, nbR, map);
+					testColor[map[i + xPos][j + yPos]] += 1;
+                }
+				else {
+					testColor[map[i + xPos][j + yPos]] += 1;
+				}
+            }
         }
-        cout << endl;
-    }
 
-    // Filtrage
-    rec_filtering(size[0], size[1], 0, 0, picture);
-
-    // Renvoie de l'image
-
-    writeData("testWrite.ppm", 255, size[0], size[1],
-              picture); // RAJOUTER LA TABLE DE COULEUR
-    cout << "done" << endl;
-
-    return (0);
+		for (int i = 0; i <= nbR; i++){
+			if (testColor[i] > maxColorNb) {
+				maxColorNb = testColor[i];
+				maxColorValue = i;
+			}
+		}
+        if (maxColorNb >= 6) {
+            map[xPos][yPos] = maxColorValue;
+        } else {
+            map[xPos][yPos] = 0;
+        }
+	}
 }
 
-void error_nbR(int nbR) { cout << "Invalid number of colors: " << nbR << endl; }
-
-void error_color(int id) { cout << "Invalid color value " << id << endl; }
-
-void error_threshold(double invalid_val) {
-    cout << "Invalid threshold value: " << invalid_val << endl;
+void error_nbR(int nbR)
+{
+	cout << "Invalid number of colors: " << nbR << endl;
 }
 
-void error_nb_filter(int nb_filter) {
-    cout << "Invalid number of filter: " << nb_filter << endl;
+void error_color(int id)
+{
+	cout << "Invalid color value " << id << endl;
+}
+
+void error_threshold(double invalid_val)
+{
+	cout << "Invalid threshold value: " << invalid_val << endl;
+}
+
+void error_nb_filter(int nb_filter)
+{
+	cout << "Invalid number of filter: " << nb_filter << endl;
 }
